626101361d4b1a97579115969b2c2cb76951260c
benjamin <benjamin.l.dayan@gmail.com>
2016-03-17 17:21:25 +0000

    added servo control

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ArmAndServoControl.java
@@ -0,0 +1,45 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by benjamin on 17/03/2016.
+ */
+public class ArmAndServoControl extends OpMode {
+
+    DcMotor leftArm;
+    DcMotor rightArm;
+
+    Servo servo;
+
+    @Override
+    public void init() {
+        leftArm = hardwareMap.dcMotor.get("left_arm");
+        rightArm = hardwareMap.dcMotor.get("right_arm");
+        leftArm.setDirection(DcMotor.Direction.REVERSE);//reversing the left motor for testing
+
+        servo = hardwareMap.servo.get("servo");
+    }
+
+    @Override
+    public void loop() {
+        /*if (gamepad2.right_stick_y < 0.0) {
+            leftArm.setPower(gamepad2.right_stick_y);
+            rightArm.setPower(gamepad2.right_stick_y);
+        }
+        else if(gamepad2.right_stick_y > 0.0) {
+            leftArm.setPower(gamepad2.right_stick_y);
+            rightArm.setPower(gamepad2.right_stick_y);
+        }
+        else {
+            leftArm.setPower(0);
+            rightArm.setPower(0);
+        }*/
+        leftArm.setPower(gamepad2.right_stick_y);
+        rightArm.setPower(gamepad2.right_stick_y);
+
+        servo.setPosition(gamepad2.left_stick_x);
+    }
+}
